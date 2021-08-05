f7fdc511bed043061fd833c4bc84acd29023f3e2
edercesar <54750966+edercesar@users.noreply.github.com>
2021-08-05 11:27:42 -0300

    Delete .vagrant directory

--- .vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["vagrant-group",["= 0.8.2"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]]],"checksum":"a3ebbbfefb6bedd5a92d39957ab1cb588faf0e04bc4bf238610b2b214270f9f8","vagrant_version":"2.2.14"}
